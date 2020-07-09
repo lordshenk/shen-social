118f10a18399b1e6d084e8965f994d3b6e89f8bd
lordshenk <61837826+lordshenk@users.noreply.github.com>
2020-07-09 09:33:50 +0700

    switch to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "./components/Auth";
 import Newsfeed from "./components/Newsfeed";
 import Following from "./components/Following";
